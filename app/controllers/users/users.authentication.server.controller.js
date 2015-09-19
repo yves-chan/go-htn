d33b0c4ce8e46161b9978d63ec3f2debbda95505
yves-chan~ <chanyves@gmail.com>
2015-09-19 06:37:14 -0700

    schema for activities and history

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -84,14 +84,14 @@ exports.oauthCallback = function(strategy) {
 	return function(req, res, next) {
 		passport.authenticate(strategy, function(err, user, redirectURL) {
 			if (err || !user) {
-				console.log("err1 is" + err);
-				console.log("user: " + user);
+				console.log('err1 is' + err);
+				console.log('user: ' + user);
 
 				return res.redirect('/#!/signin');
 			}
 			req.login(user, function(err) {
 				if (err) {
-					console.log("err2");
+					console.log('err2');
 
 					return res.redirect('/#!/signin');
 
---
+++ app/models/history.server.model.js
@@ -0,0 +1,65 @@
+/**
+ * Created by yves on 19/09/15.
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/**
+ * Activity Schema
+ */
+var HistorySchema = new Schema({
+    username: String,
+    date: {
+        type:Date
+    },
+    activity: String
+}, {collection:'histories'});
+
+
+function History(history) {
+    this.username = history.name;
+    this.date = history.date;
+    this.activity = history.activity;
+}
+
+var historyModel = mongoose.model('History', HistorySchema);
+
+/*
+ Saves an admin to the admin database
+ */
+History.prototype.save = function() {
+    var history = {
+        username: this.username,
+        date: this.date,
+        activity: this.activity
+    };
+    var newHistory = new historyModel(history);
+    newHistory.save(function(err) {
+        if (err) {
+            console.error('History save failed [history.server.model.js]');
+        }
+    });
+};
+
+/**
+ * Find all histories for a user
+ */
+History.getAll = function(username, callback) {
+    historyModel.find(
+        {username: username},
+        function(err, doc) {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, doc);
+        });
+};
+
+module.exports = History;
+module.exports.historyModel = historyModel;
